dfcb66bb28639479c0cfa153417a47783517e764
tomoh11.0223soccer@gmail.com <tomoh11.0223soccer@gmail.com>
2026-05-17 14:07:56 +0900

    フロントエンドの初期設定

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>Homeコンポーネントです</div>
+  )
+}
+
+export default Home
---
+++ frontend/src/pages/Result/Result.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Result() {
+  return (
+    <div>分析結果をこのコンポーネントに表示します。</div>
+  )
+}
+
+export default Result
